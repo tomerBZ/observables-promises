aa5cb1c001d1ae446dc0f718882cc78d0d760067
Tomer David Ben Zohar <tomer@wisegroup.co.il>
2018-02-28 21:35:56 +0200

    New approach

--- src/app/observable/observable.component.ts
+++ src/app/observable/observable.component.ts
@@ -1,7 +1,4 @@
-import 'rxjs/add/operator/delay';
-import 'rxjs/add/operator/retryWhen';
-import 'rxjs/add/operator/switchMap';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Follower } from '../shared/interfaces/follower';
 import { HttpService } from '../shared/services/http/http.service';
 
@@ -11,26 +8,20 @@ import { HttpService } from '../shared/services/http/http.service';
   templateUrl: './observable.component.html',
   styleUrls: ['./observable.component.scss']
 })
-export class ObservableComponent implements OnInit {
+export class ObservableComponent implements OnInit, OnDestroy {
   followers: Follower[];
 
   constructor(private _http: HttpService) {
   }
 
   ngOnInit() {
-    this._http.getFollowers()
-      .retryWhen((error) => error.delay(5000))
-      .do(data => {
-        if (data.is_more) {
-          this._http.getFollowers(data.next_page).subscribe(newData => {
-            if (this.followers.length) {
-              this.followers = this.followers.concat(newData.followers);
-            }
-          });
-        }
-      })
-      .subscribe(response => {
-        this.followers = response.followers;
-      }, error => console.log(error));
+    this._http.followers.subscribe(response => {
+      this.followers = response;
+    }, error => console.log(error));
+    this._http.loadAll();
+  }
+
+  ngOnDestroy() {
+    this._http.dataStore = {followers: []};
   }
 }
--- src/app/shared/services/http/http.service.ts
+++ src/app/shared/services/http/http.service.ts
@@ -1,24 +1,47 @@
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/toPromise';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { FollowersResponse } from '../../interfaces/followers-response';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/toPromise';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Follower } from '../../interfaces/follower';
 
 @Injectable()
 export class HttpService {
+  followers: Observable<Follower[]>;
+  private _followers: BehaviorSubject<Follower[]>;
+  public dataStore: {
+    followers: Follower[]
+  };
 
   constructor(private http: HttpClient) {
+    this.dataStore = {followers: []};
+    this._followers = <BehaviorSubject<Follower[]>>new BehaviorSubject([]);
+    this.followers = this._followers.asObservable();
   }
 
-  getFollowers(url?: string): Observable<FollowersResponse> {
-    return this.http.get<FollowersResponse>(typeof url !== 'undefined' ? url : 'http://localhost:4200/assets/data/followers.json')
-      .map(response => response)
-      .catch((error) => Observable.throw(error));
+
+  loadAll(url?: string) {
+    this.http.get<FollowersResponse>(typeof url !== 'undefined' ? url : 'http://localhost:4200/assets/data/followers.json')
+      .do(data => {
+        if (data.next_page) {
+          this.dataStore.followers = !this.dataStore.followers.length ? data.followers : this.dataStore.followers.concat(data.followers);
+          this.loadAll(data.next_page);
+        } else {
+          this._followers.next(this.dataStore.followers.concat(data.followers))
+        }
+      })
+      .subscribe(data => {
+        // console.log(data);
+        },
+        error => console.log('Could not load json.', error),
+        // () => console.log('complete')
+      );
   }
 
   getFollowersPromise(url?: string): Promise<FollowersResponse> {
